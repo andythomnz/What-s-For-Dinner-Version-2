c450c9fc9ae42037fa277e437aa1ce9dcb9243b7
Andrew Thompson <thompsandr4@ecs.vuw.ac.nz>
2018-09-29 09:36:29 +1200

    Removed no-longer used Settings screen

--- WhatsForDinner/AppNavigator.js
+++ WhatsForDinner/AppNavigator.js
@@ -1,7 +1,7 @@
 import { createStackNavigator } from 'react-navigation';
 import Home from './Home';
 import Meals from './Meals';
-import Settings from './Settings';
+//import Settings from './Settings';
 import Tabs from './Tabs';
 import NewMeal from './NewMeal';
 import ViewMeal from './ViewMeal';
@@ -12,7 +12,6 @@ const AppNavigator = createStackNavigator({
 
   Home: { screen: Home },
   Meals: { screen: Meals},
-  Settings: { screen: Settings},
   Tabs: {screen: Tabs},
   NewMeal: {screen: NewMeal},
   ViewMeal: { screen: ViewMeal },
